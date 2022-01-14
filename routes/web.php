07b0ac2eb885b87b27bd7a5c6d88fb4047548201
Philippe Trudel <ptr@signifly.com>
2022-01-14 14:27:26 -0500

    added web.php methods

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Monster;
-
+use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +29,26 @@ Route::get('/monsters/{id}', function ($id) {
     $monsters = Monster::find($id);
     return response($monsters, 200);
 })->where('id', '[0-9]+');
+
+//create
+// POST
+Route::post('/monsters', function(Request $request) {
+    // instance the object
+    $monster = new Monster();
+
+    // set the request propreties to the monster instance
+    dd($request);
+
+    // save
+});
+
+//update
+// PUT
+Route::put();
+
+// Route::patch()
+
+
+//delete
+// DELETE
+Route::delete();
